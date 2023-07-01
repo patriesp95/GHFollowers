71946d427224a19c8f97954bf7fbdbc565a82ddd
Patricia Martínez Espert <patrimespert@outlook.com>
2023-07-01 17:57:46 +0200

    Custom Views - UIButton & UITextField

---
+++ GHFollowers/GFTextfield.swift
@@ -0,0 +1,42 @@
+//
+//  GFTextfield.swift
+//  GHFollowers
+//
+//  Created by Patricia Martínez Espert on 1/7/23.
+//
+
+import UIKit
+
+class GFTextfield: UITextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure(){
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Enter a username"
+    }
+    
+}
