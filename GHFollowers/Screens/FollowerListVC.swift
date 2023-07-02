d5bd791e7d1581330e74d98bf89e0eff302f9a52
Patricia Martínez Espert <patrimespert@outlook.com>
2023-07-02 19:16:57 +0200

    Create Network Call - Get Followers

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -16,6 +16,15 @@ class FollowerListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
 
+        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: errorMessage!, buttonTitle: "Ok")
+                return
+            }
+            
+            print(followers)
+        }
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
