162879a25a3b6c53931cd6a40fa42a145ad408a3
Patricia Martínez Espert <patrimespert@outlook.com>
2023-07-02 15:37:32 +0200

    Planning our Network Calls - API, JSON, Model

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Patricia Martínez Espert on 2/7/23.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
