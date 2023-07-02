fe824550ef00c707a39f2cc897399752965df423
Patricia Martínez Espert <patrimespert@outlook.com>
2023-07-02 09:58:57 +0200

    revert test

--- GHFollowers/GFAlertVC.swift
+++ GHFollowers/GFAlertVC.swift
@@ -47,7 +47,7 @@ class GFAlertVC: UIViewController {
         containerView.layer.cornerRadius = 16
         containerView.layer.borderWidth = 2
         containerView.layer.borderColor = UIColor.white.cgColor
-        containerView.translatesAutoresizingMaskIntoConstraints = true
+        containerView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
