67ea557b8688d8832d30c8eb0d220c4c82fd954d
Patricia Martínez Espert <patrimespert@outlook.com>
2023-07-04 20:22:30 +0200

    Downloading & Caching Avatar Images

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Patricia Martínez Espert on 2/7/23.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com"
+    private let baseURL = "https://api.github.com"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
