85288d72f49d3aeb1616f4a1d5ba6f0e218d28cc
Patricia Martínez Espert <patrimespert@outlook.com>
2023-07-04 23:06:36 +0200

    Loading View

--- GHFollowers/Extension/UIViewController+Ext.swift
+++ GHFollowers/Extension/UIViewController+Ext.swift
@@ -7,7 +7,10 @@
 
 import UIKit
 
+fileprivate var containerView: UIView!
+
 extension UIViewController {
+    
     func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String){
         DispatchQueue.main.async {
             let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
@@ -16,4 +19,36 @@ extension UIViewController {
             self.present(alertVC, animated: true)
         }
     }
+    
+    func showLoadingView(){
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) { containerView.alpha = 0.8 }
+        
+        let activityIndicator = UIActivityIndicatorView(style:.large)
+        containerView.addSubview(activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+        
+        activityIndicator.startAnimating()
+    }
+    
+    func dismissLoadingView(){
+        
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
+        }
+     
+    }
+    
 }
--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -46,9 +46,10 @@ class FollowerListVC: UIViewController {
     }
     
     func getFollowers(username: String, page: Int){
+        self.showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
-            
+            self.dismissLoadingView()
             switch result {
                 case .success(let followers):
                 guard let followers = followers else { return }
